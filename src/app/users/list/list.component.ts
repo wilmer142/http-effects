7036c696bc702de988614d92ab3f82dadbf97030
wilmer142 <wilmercollege@gmail.com>
2019-12-25 10:51:21 -0500

    Designing list component html with user data

--- src/app/users/list/list.component.ts
+++ src/app/users/list/list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-list',
@@ -8,11 +9,14 @@ import { UserService } from '../../services/user.service';
 })
 export class ListComponent implements OnInit {
 
+  users: User[] = [];
+
   constructor(public userService: UserService) { }
 
   ngOnInit() {
-    this.userService.getUsers().subscribe(data => {
-      console.log(data);
+    this.userService.getUsers().subscribe(users => {
+      console.log(users);
+      this.users = users;
     });
   }
 
