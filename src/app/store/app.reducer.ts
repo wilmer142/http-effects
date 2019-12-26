423c152865c45420ee30743830075850ab47a2c4
wilmer142 <wilmercollege@gmail.com>
2019-12-25 21:36:14 -0500

    Adding user state to app reducer

--- src/app/store/app.reducer.ts
+++ src/app/store/app.reducer.ts
@@ -1,11 +1,14 @@
 import { UsersState, usersReducer } from './reducers/users.reducer';
 import { ActionReducerMap } from '@ngrx/store';
+import { UserState, userReducer } from './reducers/user.reducer';
 
 
 export interface AppState {
     users: UsersState;
+    user: UserState;
 }
 
 export const appReducers: ActionReducerMap<AppState> = {
-    users: usersReducer
+    users: usersReducer,
+    user: userReducer
 };
