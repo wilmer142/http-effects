f74fcd678c1aa4ec5c14d5c72420b882d75b975a
wilmer142 <wilmercollege@gmail.com>
2019-12-25 22:12:09 -0500

    Designing user htmll and subscribe to user actions

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app.reducer';
 import { LoadUser } from '../../store/actions/user.actions';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-user',
@@ -11,12 +12,22 @@ import { LoadUser } from '../../store/actions/user.actions';
 })
 export class UserComponent implements OnInit {
 
+  user: User;
+  loading: boolean;
+  error: any;
+
   constructor(private router: ActivatedRoute, private store: Store<AppState>) { }
 
   ngOnInit() {
     this.router.params.subscribe(params => {
       const id = params['id'];
       this.store.dispatch(new LoadUser(id));
+    });
+
+    this.store.select('user').subscribe(user => {
+      this.user = user.user;
+      this.loading = user.loading;
+      this.error = user.error;
     })
   }
 
