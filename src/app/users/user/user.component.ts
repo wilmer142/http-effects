f3b380f567ce19434a3bfbeae42f56ced58a9771
wilmer142 <wilmercollege@gmail.com>
2019-12-25 21:41:42 -0500

    dispatch load user action from user component

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducer';
+import { LoadUser } from '../../store/actions/user.actions';
 
 @Component({
   selector: 'app-user',
@@ -8,11 +11,12 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
 
-  constructor(private router: ActivatedRoute) { }
+  constructor(private router: ActivatedRoute, private store: Store<AppState>) { }
 
   ngOnInit() {
     this.router.params.subscribe(params => {
       const id = params['id'];
+      this.store.dispatch(new LoadUser(id));
     })
   }
 
